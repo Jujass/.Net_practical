56767d92a543dc3f78902b550bb6b6279b2580b5
Juta Jasiulioniene <neviadomskyte@gmail.com>
2022-11-13 18:00:05 +0200

    Initial commit

--- HelloWorldVS/VSIntro/Program.cs
+++ HelloWorldVS/VSIntro/Program.cs
@@ -1,9 +1,32 @@
 ﻿
+using System;
+
 class VSIntro
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
+        //int intvariable = 20;
+        ////// See https://aka.ms/new-console-template for more information
+        ////Console.WriteLine("Hello, World2!");
+        ////Console.WriteLine("Hello, World3!");
+        //Console.WriteLine(intvariable
+        //intvariable = 18;
+        //Console.WriteLine(intvariable)
+        ////intvariable = 27;
+        //Console.WriteLine(intvariable)
+
+        string name = "Jhon";
+        uint age = 18;///unsigned integer, because the age can't be negative
+        //int age = 18;///integer
+        char gender = 'M';
+        float height = 1.87f;
+        bool underaged, smoking;
+        //Console.WriteLine(name);
+        //Console.WriteLine(age);
+        //Console.WriteLine(gender);
+        //Console.WriteLine(height);
+        //Console.WriteLine(name + " " + age + " " + gender + " " + height);
+        Console.WriteLine(name + '\n' + age + '\n' + gender + '\n' + height);
+        
     }
 }
