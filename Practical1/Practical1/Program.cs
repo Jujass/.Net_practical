f3a9c9928f435e598f56dd4db18598bf1546c6d9
Juta Jasiulioniene <neviadomskyte@gmail.com>
2022-11-23 18:54:51 +0200

    doc added

--- Practical1/Practical1/Program.cs
+++ Practical1/Practical1/Program.cs
@@ -6,17 +6,13 @@
         Console.WriteLine("Enter first number");
         string num1 = Console.ReadLine();
         Console.WriteLine("Enter second number");
-        string num2 = Console.ReadLine();
-        int number1 = Int32.Parse(num1);
-        int number2 = Int32.Parse(num2);
-        int addition = number1 + number2;
-        Console.WriteLine("Addition: " + addition);
-        int substraction = number1 - number2;
-        Console.WriteLine("Substraction: " + substraction);
-        int multiplication = number1 * number2;
-        Console.WriteLine("Multiplication: " + multiplication);
-        int division = number1 / number2;
-        Console.WriteLine("Division: " + division);
-
+      string num2 = Console.ReadLine();
+        string result = num1 + num2;
+        Console.WriteLine(result);
+        
+    
     }
 }
+public static readonly
+    System.ComponentModel.DataAnnotations.ValidationResult
+
