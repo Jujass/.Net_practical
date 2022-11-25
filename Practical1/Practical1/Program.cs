915ca478b578973e7cdb3dc48867d1b608f6aae3
Juta Jasiulioniene <neviadomskyte@gmail.com>
2022-11-25 13:10:36 +0200

    changes done

--- Practical1/Practical1/Program.cs
+++ Practical1/Practical1/Program.cs
@@ -2,16 +2,38 @@
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Enter number 1:");
+        string number1str = Console.ReadLine();
 
-        Console.WriteLine("Enter first number");
-        string num1 = Console.ReadLine();
-        Console.WriteLine("Enter second number");
-      string num2 = Console.ReadLine();
-        string result = num1 + num2;
-        Console.WriteLine(result);
-        
-    
+        Console.WriteLine("Enter number 2:");
+        string number2str = Console.ReadLine();
+
+        int number1 = int.Parse(number1str);
+        int number2 = int.Parse(number2str);
+
+        Console.WriteLine("Addition:");
+        int addition = number1 + number2;
+        Console.WriteLine(number1 + "+" + number2 + "=" + addition);
+
+        Console.WriteLine("Substraction:");
+        int substraction = number1 - number2;
+        Console.WriteLine(number1 + "-" + number2 + "=" + substraction);
+
+        Console.WriteLine("Multiplication:");
+        int multiplication = number1 * number2;
+        Console.WriteLine(number1 + "*" + number2 + "=" + multiplication);
+
+        double num1 = Double.Parse(number1str);
+        double num2 = Double.Parse(number2str);
+
+        Console.WriteLine("Division:");
+        double division = num1 / num2;
+        Console.WriteLine(number1 + "/" + number2 + "=" + division);
+
+
+
+
+
     }
+
 }
-
-
